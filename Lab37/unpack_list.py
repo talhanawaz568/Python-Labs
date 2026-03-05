3b9c42730a0713415ed66cc533c7458b9b73d2c8
talhanawaz568 <alnafi789@gmail.com>
2026-03-05 10:59:22 +0000

    first commit

--- Lab37/unpack_list.py
+++ Lab37/unpack_list.py
@@ -1,9 +1,14 @@
+def sum_numbers(*args):
+    return sum(args)
+
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+def show_info(**kwargs):
+    for k, v in kwargs.items():
+        print(f"{k} is {v}")
+
+# Now your existing code will work
 numbers = [1, 2, 3, 4]
 print(sum_numbers(*numbers))
-
-
-data = {'name': 'John', 'age': 25}
-print_kwargs(**data)
-
-info = {'occupation': 'Engineer', 'country': 'USA'}
-show_info(**info)
