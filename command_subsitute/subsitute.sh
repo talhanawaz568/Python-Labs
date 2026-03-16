e0b35ad65e54f75ed974053a699d99ab75236ef8
talhanawaz568 <alnafi789@gmail.com>
2026-03-16 17:26:21 +0000

    first commit

---
+++ command_subsitute/subsitute.sh
@@ -0,0 +1,11 @@
+CURRENT_DATE=$(date)
+echo "Today is $CURRENT_DATE"
+
+USER_NAME=$(whoami)
+echo "Current user: $USER_NAME"
+CURRENT_DIR=$(pwd)
+echo "Current directory: $CURRENT_DIR"
+
+KERNEL_VERSION=$(uname -r)
+echo "Kernel Version: $KERNEL_VERSION"
+
---
+++ debug_log/multiline_output.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Welcome to the Logging and Redirecting Output Lab!"
+echo "This script demonstrates output management."
+echo "Each message will be logged to a file and displayed on the console."
+
+
