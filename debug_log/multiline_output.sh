3b787a9553c51db77ca6ec558acf320c68432135
Muhammad Talha Nawaz <alnafi789@gmail.com>
2026-03-18 00:15:10 +0500

    Delete debug_log directory

--- debug_log/multiline_output.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Welcome to the Logging and Redirecting Output Lab!"
-echo "This script demonstrates output management."
-echo "Each message will be logged to a file and displayed on the console."
-
-
